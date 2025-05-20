b24be5890b8e6cfbdb0dd0ede2022d58bbd61c14
Megha <bathlamegha6@gmail.com>
2025-05-21 02:23:46 +0530

    created dropdown box feature with custom hook

---
+++ src/component/DropDown/DropDown.tsx
@@ -0,0 +1,42 @@
+import { SlOptions } from "react-icons/sl";
+import "./DropDown.css";
+import { useState } from "react";
+import { useOutsideClick } from "../../hooks/useOutsideClick";
+
+type ListItem = {
+  label: string;
+  onClick: () => void;
+};
+type DropDownProps = {
+  list: ListItem[];
+};
+// add cursor pointer in css
+export const DropDown = ({ list }: DropDownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { ref } = useOutsideClick(() => {
+    setIsOpen(false);
+  });
+  return (
+    <div className="dropdown_container  ">
+      <SlOptions
+        onClick={() => {
+          setIsOpen((prevOption) => !prevOption);
+        }}
+      />
+
+      {isOpen ? (
+        <div className="dropDown_options" ref={ref}>
+          <div>
+            {list.map(({ label, onClick }) => {
+              return (
+                <div className="dropDown_list" onClick={onClick} key={label}>
+                  {label}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
