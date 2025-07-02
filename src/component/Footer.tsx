edb0ebe2a17c35d0042081de1a94246dce96b3b9
Megha <bathlamegha6@gmail.com>
2025-07-02 22:29:27 +0530

    done with footer feature

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { IoMdSettings } from "react-icons/io";
 import { TaskWidget } from "./TaskWidget";
+import { quotesList } from "../util/quotes";
 
 const Footer = () => {
   const openChatGPT = () => {
     window.open("https://chat.openai.com/", "_blank"); // new tab
     // window.location.href = "https://chat.openai.com/"; // load on same page
   };
+  const generateRandomNum = () => {
+    const randonNum = Math.floor(Math.random() * 10);
+    return randonNum.toString();
+  };
+  const randomNumber = generateRandomNum();
+  const matchedQuote = quotesList.find(
+    (quote) => quote.quoteNum === randomNumber
+  );
+
   return (
     <div className="footer">
       <div className=" settings_section">
@@ -14,8 +24,9 @@ const Footer = () => {
         <div onClick={openChatGPT}>Ask AI</div>
       </div>
       <div>
-        “Enjoy the little things, for one day you may look back and realize they
-        were the big ones.”
+        {matchedQuote
+          ? matchedQuote.quotation
+          : "“Enjoy the little things, for one day you may look back and realize they were the big ones.”"}
       </div>
       <TaskWidget />
     </div>
