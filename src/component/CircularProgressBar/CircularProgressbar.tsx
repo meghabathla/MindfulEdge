edf235a1ca8e0c8c5a1c54db94f6da85d372c47a
Megha <bathlamegha6@gmail.com>
2025-07-10 23:30:08 +0530

    done

---
+++ src/component/CircularProgressBar/CircularProgressbar.tsx
@@ -0,0 +1,47 @@
+const CircularProgressBar = ({
+  percentage = 0,
+  size = 120,
+  strokeWidth = 12,
+}) => {
+  const radius = (size - strokeWidth) / 2;
+  const circumference = 2 * Math.PI * radius;
+  const offset = circumference - (percentage / 100) * circumference;
+  console.log({
+    percentage,
+    size,
+    strokeWidth,
+  });
+
+  return (
+    <div style={{ position: "relative", width: size, height: size }}>
+      <svg width={size} height={size}>
+        <circle
+          stroke="rgba(255,255,255,0.15)"
+          fill="none"
+          strokeWidth={strokeWidth}
+          cx={size / 2}
+          cy={size / 2}
+          r={radius}
+        />
+        <circle
+          stroke="#edefef"
+          fill="none"
+          strokeWidth={strokeWidth}
+          strokeLinecap="round"
+          cx={size / 2}
+          cy={size / 2}
+          r={radius}
+          style={{
+            strokeDasharray: circumference,
+            strokeDashoffset: offset,
+            transform: "rotate(-90deg)",
+            transformOrigin: "50% 50%",
+            transition: "stroke-dashoffset 0.35s ease",
+          }}
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default CircularProgressBar;
