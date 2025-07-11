4dee2c4611d3597eb5ab28b83e7ffa3aeb71835c
Megha <bathlamegha6@gmail.com>
2025-07-12 00:32:27 +0530

    make the ui of Break and focus btn

---
+++ src/component/FocusBreakBtn/FocusBreakBtn.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./FocusBreakBtn.css";
+
+const FocusBreakBtn = () => {
+  return (
+    <div className="focus_break_button">
+      <div className="focus_btn">FOCUS</div>
+      <div className="break_btn">BREAK</div>
+    </div>
+  );
+};
+
+export default FocusBreakBtn;
