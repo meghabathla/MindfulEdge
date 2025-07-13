7625c33c277a31e0f9ebb9590b79c8641f5402ab
Megha <bathlamegha6@gmail.com>
2025-07-13 16:44:16 +0530

    toggle play pause button

--- src/component/PlayPause/PlayPause.tsx
+++ src/component/PlayPause/PlayPause.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoPlay } from "react-icons/io5";
 import { PiPauseFill } from "react-icons/pi";
 import "./PlayPause.css";
 
 const PlayPause = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div className="play_pause_button">
-      <PiPauseFill className="pause_icon" />
-      <IoPlay className="play_icon" />
+      {isPlaying ? (
+        <IoPlay className="play_icon" onClick={togglePlayPause} />
+      ) : (
+        <PiPauseFill className="pause_icon" onClick={togglePlayPause} />
+      )}
     </div>
   );
 };
