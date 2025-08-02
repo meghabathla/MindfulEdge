9d435c9875f81185df574c07f402e1d83dab2bfe
Megha <bathlamegha6@gmail.com>
2025-08-02 23:24:28 +0530

    added reset button

--- src/component/PlayPause/PlayPause.tsx
+++ src/component/PlayPause/PlayPause.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { IoPlay } from "react-icons/io5";
 import { PiPauseFill } from "react-icons/pi";
+import { VscDebugRestart } from "react-icons/vsc";
 import "./PlayPause.css";
 import { useFocusContext } from "../../store/FocusContext/FocusContext";
 
 const PlayPause = () => {
-  const { isFocusing } = useFocusContext();
+  const { isFocusing, startFocus, stopFocus, restartFocus } = useFocusContext();
   console.log(isFocusing);
 
   return (
     <div className="play_pause_button">
       {isFocusing ? (
-        <PiPauseFill className="pause_icon" />
+        <PiPauseFill className="pause_icon" onClick={stopFocus} />
       ) : (
-        <IoPlay className="play_icon" />
+        <IoPlay className="play_icon" onClick={startFocus} />
       )}
+      <VscDebugRestart className="restart_icon" onClick={restartFocus} />
     </div>
   );
 };
