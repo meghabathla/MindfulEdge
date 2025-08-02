795aa7646fc09df76bb8ddcceb768c9a952d4e42
Megha <bathlamegha6@gmail.com>
2025-08-02 23:24:58 +0530

    removed break btn

--- src/component/FocusBreakBtn/FocusBreakBtn.tsx
+++ src/component/FocusBreakBtn/FocusBreakBtn.tsx
@@ -1,24 +1,10 @@
 import React from "react";
 import "./FocusBreakBtn.css";
-import { useFocusContext } from "../../store/FocusContext/FocusContext";
 
 const FocusBreakBtn = () => {
-  const { isBreakMode, toggleBreakMode } = useFocusContext();
-
   return (
     <div className="focus_break_button">
-      <div
-        className={`focus_btn ${!isBreakMode ? "active" : ""}`}
-        onClick={() => !isBreakMode && toggleBreakMode()}
-      >
-        FOCUS
-      </div>
-      <div
-        className={`break_btn ${isBreakMode ? "active" : ""}`}
-        onClick={() => isBreakMode && toggleBreakMode()}
-      >
-        BREAK
-      </div>
+      <div className={`focus_btn`}>FOCUS MODE</div>
     </div>
   );
 };
