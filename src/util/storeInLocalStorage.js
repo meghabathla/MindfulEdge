239cdef4f18fb2dee5ba726aa078ef9570dffeef
Megha <bathlamegha6@gmail.com>
2025-08-02 23:38:24 +0530

    removed this file from utils

--- src/util/storeInLocalStorage.js
+++
@@ -1,5 +0,0 @@
-const storage = {
-  set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
-  get: (key) => JSON.parse(localStorage.getItem(key) || "null"),
-  remove: (key) => localStorage.removeItem(key),
-};
